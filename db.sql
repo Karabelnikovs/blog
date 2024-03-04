19ba669c7e93e72c15fcb278f13be54d7789ecaf
Karabelnikovs <125544102+Karabelnikovs@users.noreply.github.com>
2024-03-04 13:28:44 +0200

    commit

---
+++ db.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE blog_tavs_uzvards;
+USE blog_tavs_uzvards;
+
+CREATE TABLE posts (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title  VARCHAR(255) NOT NULL
+);
+
+INSERT INTO posts 
+(title) 
+VALUES 
+ ('My First Blog Post'), 
+ ('My Second Blog Post');
--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
+require "functions.php";
 
-echo "hlo wrld";
+$connection_string = "mysql:host=localhost;port=3306;dbname=blog_tavs_uzvards;user=root;password=;charset=utf8mb4";
+
+$connection = new PDO($connection_string);
+
+//sagatavot sql vaicajumu
+$query = $connection->prepare("SELECT * FROM posts");
+
+//izpildit sql vaicajumu
+$query->execute();
+
+//sanemt datus no sql uz php
+$posts = $query->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<h1>Posts</h1>";
+
+
+foreach($posts as $post){
+    echo "<li>" . $post["title"] . "</li>" . "<br>";
+}
