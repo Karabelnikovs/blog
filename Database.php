fd19486411e36bb662c99a8da3f83018fdb6f3b6
Karabelnikovs <125544102+Karabelnikovs@users.noreply.github.com>
2024-03-04 13:43:36 +0200

    commit

--- Database.php
+++ Database.php
@@ -1,7 +1,7 @@
 <?php
 
 class Database{
-    public function do(){
+    public function execute(){
         $connection_string = "mysql:host=localhost;port=3306;dbname=blog_tavs_uzvards;user=root;password=;charset=utf8mb4";
 
 $connection = new PDO($connection_string);
